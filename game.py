94b908605582d53f01dca95d5f5f2779332b7f66
LexinQiu <97572814+LexinQiu@users.noreply.github.com>
2022-02-04 22:48:47 -0500

    Update game.py

--- game.py
+++ game.py
@@ -38,17 +38,17 @@ print("Computer chose:", computer_choice)
 
 # determine the winner
 
-if u == computer_choice:
+if v == computer_choice:
     print("It's a tie!")
-elif u == "rock" and computer_choice == "paper":
+elif v == "rock" and computer_choice == "paper":
     print("Oh, the computer won. It's ok.")
-elif u == "rock" and computer_choice == "scissors":
+elif v == "rock" and computer_choice == "scissors":
     print("Congratulations! You won!")
-elif u == "scissors" and computer_choice == "rock":
+elif v == "scissors" and computer_choice == "rock":
     print("Oh, the computer won. It's ok.")
-elif u == "scissors" and computer_choice == "paper":
+elif v == "scissors" and computer_choice == "paper":
     print("Congratulations! You won!")
-elif u == "paper" and computer_choice == "scissors":
+elif v == "paper" and computer_choice == "scissors":
     print("Oh, the computer won. It's ok.")
 else:    
     print("Oh, the computer won. It's ok.")
@@ -57,7 +57,9 @@ else:
 # display final results
 print ("Thanks for playing. Please play again!")
 
-#player name customization
+# player name customization
 import os
 
 player_name = os.getenv("PLAYER_NAME", default="Player One")
+
+
